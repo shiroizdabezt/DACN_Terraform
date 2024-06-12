aa0890f14ccdaa3c7d59ff5ac5a493b65a4fffbe
Shiroizdabezt <codykhoanguyen@gmail.com>
2024-06-12 22:09:33 +0700

    test bug docker did'nt be installed

--- backend.sh
+++ backend.sh
@@ -1,20 +1,10 @@
-sudo apt-get update
-sudo apt-get install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-# Add the repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo apt-get update
-sudo docker pull pokilee10/backend:latest
-sudo docker run -d -p 4000:4000 pokilee10/backend
 sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https curl
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
 sudo apt update
 sudo apt install caddy
+sudo apt update
+sudo apt install docker.io -y
+docker --version
+sudo docker pull pokilee10/backend:latest
+sudo docker run -d -p 4000:4000 pokilee10/backend
