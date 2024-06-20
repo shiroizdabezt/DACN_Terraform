f61eba28d9f76a54bf024afa4f3ef5f19999a674
Shiroizdabezt <codykhoanguyen@gmail.com>
2024-06-21 02:03:35 +0700

    test route 53

---
+++ route53.tf
@@ -0,0 +1,35 @@
+resource "aws_route53_record" "frontend" {
+  zone_id = aws_route53_zone.tuilalinh_zone.id
+  name    = "tuilalinh.id.vn"
+  type    = "A"
+  ttl     = 300
+  records = [aws_eip.public_ip_frontend.public_ip]
+}
+
+resource "aws_route53_zone" "tuilalinh_zone" {
+  name = "tuilalinh.id.vn"
+}
+
+resource "aws_route53_record" "backend" {
+  zone_id = aws_route53_zone.apituilalinh_zone.id
+  name    = "api.tuilalinh.id.vn"
+  type    = "A"
+  ttl     = 300
+  records = [aws_eip.public_ip_backend.public_ip]
+}
+
+resource "aws_route53_zone" "apituilalinh_zone" {
+  name = "api.tuilalinh.id.vn"
+}
+
+resource "aws_route53_record" "admin" {
+  zone_id = aws_route53_zone.admintuilalinh_zone.id
+  name    = "admin.tuilalinh.id.vn"
+  type    = "A"
+  ttl     = 300
+  records = [aws_eip.public_ip_admin.public_ip]
+}
+
+resource "aws_route53_zone" "admintuilalinh_zone" {
+  name = "admin.tuilalinh.id.vn"
+}
