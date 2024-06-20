473cbd6e35cd179fced8bd211c052e8933b2e900
Shiroizdabezt <codykhoanguyen@gmail.com>
2024-06-20 22:36:46 +0700

    test s3 bucket

--- provider.tf
+++ provider.tf
@@ -11,41 +11,41 @@ provider "aws" {
   region     = "us-west-2"
 }
 
-# resource "aws_s3_bucket" "bucket" {
-#   bucket = "mybucket"
+resource "aws_s3_bucket" "bucket" {
+  bucket = "mybucket"
 
-#   region = "us-east-1"
+  region = "us-east-1"
 
-#   tags = {
-#     Name = "My bucket"
+  tags = {
+    Name = "My bucket"
+  }
+}
+
+resource "aws_s3_bucket_object" "file" {
+  bucket = aws_s3_bucket.bucket.id
+  key    = "state/terraform.tfstate"
+}
+
+
+# terraform {
+#   backend "s3" {
+#     bucket = "your-unique-bucket-name"
+#     key    = "path/to/terraform.tfstate"
+#     region = "us-west-2"  # Đảm bảo vùng khớp với bucket
 #   }
 # }
 
-# resource "aws_s3_bucket_object" "file" {
-#   bucket = aws_s3_bucket.bucket.id
-#   key    = "state/terraform.tfstate"
-# }
-
-
-terraform {
-  backend "s3" {
-    bucket = "your-unique-bucket-name"
-    key    = "path/to/terraform.tfstate"
-    region = "us-west-2"  # Đảm bảo vùng khớp với bucket
-  }
-}
-
-# Tạo S3 bucket ở vùng chính xác
-resource "aws_s3_bucket" "tf_state_bucket" {
-  bucket = "your-unique-bucket-name"
-  acl    = "private"
+# # Tạo S3 bucket ở vùng chính xác
+# resource "aws_s3_bucket" "tf_state_bucket" {
+#   bucket = "your-unique-bucket-name"
+#   acl    = "private"
   
-  versioning {
-    enabled = true
-  }
+#   versioning {
+#     enabled = true
+#   }
 
-  tags = {
-    Name = "Terraform State Bucket"
-    Environment = "Development"
-  }
-}
+#   tags = {
+#     Name = "Terraform State Bucket"
+#     Environment = "Development"
+#   }
+# }
